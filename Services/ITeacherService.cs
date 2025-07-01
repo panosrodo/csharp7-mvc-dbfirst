6eb8700380cb4ee19aa7f759e0917da41cb266d5
panosrodo <brandonroots@hotmail.gr>
2025-07-01 07:26:48 +0300

    Teacher Service API created

---
+++ Services/ITeacherService.cs
@@ -0,0 +1,14 @@
+﻿using SchoolApp.Data;
+using SchoolApp.DTO;
+
+namespace SchoolApp.Services
+{
+    public interface ITeacherService
+    {
+        Task SignUpUserAsync(TeacherSignupDTO request);
+        Task<List<User>> GetAllUsersTeachersAsycn();
+        Task<List<User>> GetAllUsersTeachersAsycn(int pageNumber, int pageSize);
+        Task<int> GetTeacherCountAsycn();
+        Task<User?> GetTeacherByUsernameAsycn(string username);
+    }
+}
